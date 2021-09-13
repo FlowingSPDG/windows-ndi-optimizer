bccd2751dc6fd8df404524721df1fc36097400d9
Shugo Kawamura <kawamura@giken.tv>
2021-09-13 21:45:25 +0900

    Update registry.go

--- task/registry.go
+++ task/registry.go
@@ -7,8 +7,8 @@ import (
 // Optimize NIC driver via registry
 
 const (
-	registryNICSettingsPath = "HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}"
-	// registryNetworkGroupSettingsPath = ""
+	registryNICSettingsPath          = "HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4d36e972-e325-11ce-bfc1-08002be10318}"
+	registryNetworkGroupSettingsPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles"
 )
 
 // taskDisableEEE Registry optimize task. Implements Optimzier interface
